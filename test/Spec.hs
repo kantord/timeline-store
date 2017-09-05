e02fe7783b689e0153fa960d9896a6d72ae05909
Dániel Kántor <git@daniel-kantor.com>
2017-09-05 16:53:01 +0200

    Fixed test config

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,8 @@
+import Test.Tasty
+import Test.Tasty.HUnit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "unit tests" []
